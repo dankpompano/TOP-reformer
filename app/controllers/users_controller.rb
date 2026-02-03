f3fd43710a4a6145df70cc6b6e50b197b20e8e70
Jacob <jacoblittler2003@gmail.com>
2026-02-02 22:44:47 -0500

    implemented update and edit view page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,23 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to new_user_path
     else
+      puts @user.errors.full_messages
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      puts @user.errors.full_messages
+      render :edit, status: :unprocessable_entity
     end
   end
 
